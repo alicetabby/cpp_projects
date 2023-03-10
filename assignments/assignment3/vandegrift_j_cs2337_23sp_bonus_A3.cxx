dac7e7dfaaee1a718488c317f0288231f7f03567
alicetabby <Jacobtrippy124@gmail.com>
2023-03-10 03:35:02 -0600

    update

--- assignments/assignment3/vandegrift_j_cs2337_23sp_bonus_A3.cxx
+++ assignments/assignment3/vandegrift_j_cs2337_23sp_bonus_A3.cxx
@@ -15,50 +15,47 @@ using namespace std;
 
 double min(double A[], size_t n)
 {
-    double SMALLEST = A[0];
-    for (int i = 0; i < n; ++i) {
-        if (A[i] < SMALLEST)
-            SMALLEST = A[i];
-    }
-    return SMALLEST;
+        double SMALLEST = A[0];
+        for (size_t i = 0; i < n; ++i) {
+            if (A[i] < SMALLEST)
+                SMALLEST = A[i];
+        }
+        return SMALLEST;
 }
 
 double max(double A[], size_t n)
 {
-    double LARGEST = A[0];
-    for (int i = 0; i < n; ++i) {
-        if (A[i] > LARGEST)
-            LARGEST = A[i];
-    }
-    return LARGEST;
+        double LARGEST = A[0];
+        for (size_t i = 0; i < n; ++i) {
+            if (A[i] > LARGEST)
+                LARGEST = A[i];
+        }
+        return LARGEST;
 }
 
 double med(double A[], size_t n)
 {
-    if (n%2 == 1){
-       return A[(int)(n/2)];
-    } else
-        return ((A[((int)(n / 2) - 1)] + A[((int)(n / 2))]) / 2);
+        if (n%2 == 1){
+            return A[(int)(n/2)];
+        } else
+            return ((A[((int)(n / 2) - 1)] + A[((int)(n / 2))]) / 2);
 }
 
 double avg(double A[], size_t n)
 {
-    double TOTAL = 0;
-    for (int i = 0; i < n; ++i)
-        TOTAL += A[i];
-    return (TOTAL / n);
+        double TOTAL = 0;
+        for (size_t i = 0; i < n; ++i)
+            TOTAL += A[i];
+        return (TOTAL / n);
 }
 
-//  Note: see attached pdf file that shows the standard deviation calculation
-//  Precondition: n > 0, A contains n numbers.
-//  Postcondition: The population standard deviation of the numbers in A is returned.
 double stdev(double A[], size_t n)
 {
-    double NUMERATOR = 0;
-    for (int i = 0; i < n; ++i) {
-        NUMERATOR += pow((A[i] - avg(A, n)),2);
-    }
-    return sqrt(NUMERATOR / n);
+        double NUMERATOR = 0;
+        for (size_t i = 0; i < n; ++i) {
+            NUMERATOR += pow((A[i] - avg(A, n)),2);
+        }
+        return sqrt(NUMERATOR / n);
 }
 
 // BONUS: 10 points
@@ -97,10 +94,45 @@ double stdev(double A[], size_t n)
 //
 void print_dist(double A[], size_t n)
 {
+        int SET[10] = {0};
+        string GROUP[10] = {"[0,10)", "[10,20)", "[20,30)", "[30,40)", "[40,50)", 
+                         "[50,60)", "[60,70)", "[70,80)", "[80,90)", "[90,100]"}; 
 
-    // Add your implementation code here...
+        for (size_t i = 0; i < n; ++i) {
+                if ((A[i] >= 0)&&(A[i] < 10))
+                        SET[0]++; 
+                else if ((A[i] >= 10)&&(A[i] < 20))
+                        SET[1]++;
+                else if ((A[i] >= 20)&&(A[i] < 30))
+                        SET[2]++;
+                else if ((A[i] >= 30)&&(A[i] < 40))
+                        SET[3]++;
+                else if ((A[i] >= 40)&&(A[i] < 50))
+                        SET[4]++;
+                else if ((A[i] >= 50)&&(A[i] < 60))
+                        SET[5]++;
+                else if ((A[i] >= 60)&&(A[i] < 70))
+                        SET[6]++;
+                else if ((A[i] >= 70)&&(A[i] < 80))
+                        SET[7]++;
+                else if ((A[i] >= 80)&&(A[i] < 90))
+                        SET[8]++;
+                else if ((A[i] >= 90)&&(A[i] <= 100))
+                        SET[9]++;
+        }
+        
+        for (int ELEMENT = 0; ELEMENT < 10; ++ELEMENT) {
+                if (SET[ELEMENT] != 0) {
+                        cout << GROUP[ELEMENT] << fixed << setprecision(2) << "  {" << ((double)SET[ELEMENT]/(double)n) << "} ";
+                        for (int x = 0; x < SET[ELEMENT]; ++x)
+                                cout << "*";
+                        cout << endl;
+                } else
+                        cout << GROUP[ELEMENT] << endl;
+        }
+        cout << resetiosflags(ios::fixed);
+        cout << setprecision(6);
 }
-
 
 
 // Do not modify code after this line
